0c70e5211d2d1d70dca3d86416a20461de887f68
AngadJ <angad.joneja@gmail.com>
2022-12-20 11:18:12 -0500

    school copy

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,29 @@
+<script setup>
+ 
+</script>
+
+<template>
+   <div class="company">
+     <h2> About Us: </h2>
+     <p> History behind AJ's Movies4u: </p>
+     <p> This company is self-made by Angad Joneja. AJ's Movies4u begun it's website in 2022.</p>
+     <h2> Making Movies better for everyone: </h2>
+     <p> Our company provides the current trending movies and amazing information about the films.</p>
+     <p>In 2022 AJ's Movies4u began offering people around the world the option to visit trending movies directly to their homes through the internet. </p>
+     <h2> Contact Us: </h2>
+     <p> Phone number: 1-800-733-7962 </p>
+     <p> Email: ajmovies4u2022@gmail.com </p>
+   </div>
+</template>
+
+<style scoped>
+.company {
+    color: white;
+    font-size: 15px;
+    font-family: 'Play', sans-serif;
+    position: relative;
+    bottom: 400px;
+    left: 25px;
+}
+ 
+</style>
---
+++ src/views/PurchaseView.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+ 
+<template>
+    <div>
+        <h1> Wondering what movie to watch next? <br> Here are the current trending movies, click on the movie to get some cool info!</h1>
+ 
+    </div>
+       
+</template>
+ 
+<style scoped>
+   
+   h1 {
+    color: white;
+    border-color: rgb(58, 186, 237);
+    border-width: 10px;
+    border-style: double;
+    font-family: 'Zen Dots', cursive;
+    padding: 20px;
+    font-size: 25px;
+   }
+   
+   :hover {
+    border-width: 15px;
+    border-color: white
+ 
+  }
+ 
+</style>
